8af1d8299229b78fa075092a5b9acb93727b003b
Jay Fleischer <jayhf614@gmail.com>
2013-09-04 23:15:53 -0400

    Coded Problem 1
    Wrote a class with a calculateGPA method for the problem. It works (I
    think) and has documentation.

---
+++ JPPBC/src/Problem1.java
@@ -0,0 +1,34 @@
+/**
+ * The first problem of the year!
+ * 
+ * @author Jay Fleischer
+ * @version 1.0 (9-4-13)
+ */
+public class Problem1 {
+	/**
+	 * Calculates a student's GPA given his/her grades.
+	 * 
+	 * @param grades
+	 *            - all of the grades received, separated with a single space
+	 * @return the calculated GPA
+	 */
+	public static double calculateGPA(String grades) {
+		double gradeSum = 0;
+		String[] gradeArray = grades.split(" ");
+		for (String grade : gradeArray) {
+			if (grade.startsWith("A"))
+				gradeSum += 4;
+			else if (grade.startsWith("B"))
+				gradeSum += 3;
+			else if (grade.startsWith("C"))
+				gradeSum += 2;
+			else if (grade.startsWith("D"))
+				gradeSum += 1;
+			if (grade.endsWith("+") && !grade.startsWith("A"))
+				gradeSum += .3;
+			else if (grade.endsWith("-"))
+				gradeSum -= .3;
+		}
+		return gradeSum / gradeArray.length;
+	}
+}
