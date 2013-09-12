3ef1b5579824c4fada81cae99d6328db685587f4
Jay Fleischer <fleischerj@dev-lab-17.ntd.horacemann.org>
2013-09-12 14:07:56 -0400

    Optimized Question 4

--- JPPBC/src/Question4.java
+++ JPPBC/src/Question4.java
@@ -3,18 +3,18 @@ public class Question4 {
 	public static double maxSpending(double[] amounts){
 		double max=0;
 		for(int i=0;i<amounts.length;i++){
-			double value=best(amounts,0);
+			double value=best(amounts,0,0);
 			if(value>max)
 				max=value;
 		}
 		return max;
 	}
-	private static double best(double[] amounts,double currentTotal){
+	private static double best(double[] amounts,double currentTotal,int previousIndex){
 		if(currentTotal>100.0)
 			return 0;
 		double best=currentTotal;
-		for(double d:amounts){
-			double value=best(amounts,currentTotal+d);
+		for(int i=previousIndex;i<amounts.length;i++){
+			double value=best(amounts,currentTotal+amounts[i],i);
 			if(value>best)
 				best=value;
 		}
