7618b1ea8cd481762d1d0cd1d41e446fbd2e1f8b
Jay Fleischer <fleischerj@dev-lab-17.ntd.horacemann.org>
2013-09-12 13:57:32 -0400

    Wrote and Unit Tested Question4

--- Timing +Testing/src/JPPBCTests.java
+++ Timing +Testing/src/JPPBCTests.java
@@ -19,4 +19,12 @@ public class JPPBCTests {
 		assertArrayEquals(new String[] {"JUICE$", "EAGLES", "EAGERS", "W$#RDS", "W$$RDS"},
 				Question2.wackySort(new String[] {"JUICE$", "W$#RDS", "EAGLES", "EAGERS", "W$$RDS"}));
 	}
+	@Test
+	public void testQuestion4_1(){
+		assertEquals(100.0,Question4.maxSpending(new double[]{7.32,43.19,6,12.5}),.001);
+	}
+	@Test
+	public void testQuestion4_2(){
+		assertEquals(99.95,Question4.maxSpending(new double[]{4.77,81.42,18.53}),.001);
+	}
 }
