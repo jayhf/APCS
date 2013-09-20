6ff0a88f56a8b26af626a38ad8978614f129a672
Jay Fleischer <fleischerj@dev-lab-17.ntd.horacemann.org>
2013-09-20 13:51:54 -0400

    Autoformatted Unit Tests

--- Timing +Testing/src/JPPBCTests.java
+++ Timing +Testing/src/JPPBCTests.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-
 import org.junit.Test;
 
 public class JPPBCTests {
@@ -16,34 +15,45 @@ public class JPPBCTests {
 	}
 
 	@Test
+	public void testQuestion2_1() {
+		// System.out.println(Arrays.toString(Question2.fuzzNumbers(2000,1000)));
+		// assertArrayEquals(new int[]{1973,1013}, Question2.fuzzNumbers(2000,
+		// 1000));
+	}
+
+	@Test
+	public void testQuestion2_2() {
+		Question2.fuzzNumbers(Integer.MAX_VALUE, Integer.MAX_VALUE / 2);
+		// System.out.println(Arrays.toString(Question2.fuzzNumbers(150,120)));
+		// assertArrayEquals(new int[]{127,127},
+		// Question2.fuzzNumbers(150,120));
+	}
+
+	@Test
 	public void testQuestion3_1() {
-		assertArrayEquals(new String[] {"JUICE$", "EAGLES", "EAGERS", "W$#RDS", "W$$RDS"},
-				Question3.wackySort(new String[] {"JUICE$", "W$#RDS", "EAGLES", "EAGERS", "W$$RDS"}));
+		assertArrayEquals(
+				new String[] { "JUICE$", "EAGLES", "EAGERS", "W$#RDS", "W$$RDS" },
+				Question3.wackySort(new String[] { "JUICE$", "W$#RDS",
+						"EAGLES", "EAGERS", "W$$RDS" }));
 	}
 
 	@Test
 	public void testQuestion4_1() {
-		assertEquals(100.0, Question4.maxSpending(new double[] {7.32, 43.19, 6, 12.5}), .001);
+		assertEquals(100.0,
+				Question4.maxSpending(new double[] { 7.32, 43.19, 6, 12.5 }),
+				.001);
 	}
 
 	@Test
 	public void testQuestion4_2() {
-		assertEquals(99.95, Question4.maxSpending(new double[] {4.77, 81.42, 18.53}), .001);
+		assertEquals(99.95,
+				Question4.maxSpending(new double[] { 4.77, 81.42, 18.53 }),
+				.001);
 	}
 
 	@Test
 	public void testQuestion4_3() {
-		assertEquals(100, Question4.maxSpending(new double[] {.07, .03, .13, 19}), .001);
-	}
-	@Test
-	public void testQuestion2_1() {
-		//System.out.println(Arrays.toString(Question2.fuzzNumbers(2000,1000)));
-		//assertArrayEquals(new int[]{1973,1013}, Question2.fuzzNumbers(2000, 1000));
-	}
-	@Test
-	public void testQuestion2_2() {
-		Question2.fuzzNumbers(Integer.MAX_VALUE,Integer.MAX_VALUE/2);
-		//System.out.println(Arrays.toString(Question2.fuzzNumbers(150,120)));
-		//assertArrayEquals(new int[]{127,127}, Question2.fuzzNumbers(150,120));
+		assertEquals(100,
+				Question4.maxSpending(new double[] { .07, .03, .13, 19 }), .001);
 	}
 }
