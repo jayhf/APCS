4dc5d35a17ba1afa61e9d6c7e0bc0c73dd1748c7
Jay Fleischer <fleischerj@dev-lab-17.ntd.horacemann.org>
2013-10-07 14:11:31 -0400

    Fixed Question 6
    Changed the Unit Test

--- Recursion+Matrices/src/Question6.java
+++ Recursion+Matrices/src/Question6.java
@@ -1,32 +1,60 @@
-import java.util.Arrays;
-
+/**
+ * The solution for problem 6 on the Philly Classic. It plays mine sweeper with a board and specified moves.
+ * 
+ * @author Jay Fleischer
+ * @version 10-7-13
+ */
 public class Question6 {
-
-	public static void doMove(char[][] board, char[][] result, int x, int y) {
+	/**
+	 * Does a move on the board
+	 * 
+	 * @param board
+	 *            - the board to perform the move on
+	 * @param result
+	 *            - the array to store the results in
+	 * @param x
+	 *            - the x coordinate of the move
+	 * @param y
+	 *            - the y coordinate of the move
+	 * @return - whether the player lost
+	 */
+	public static boolean doMove(char[][] board, char[][] result, int x, int y) {
 		if (result[x][y] == '_')
-			if (board[x][y] == '*')
+			if (board[x][y] == '*') {
 				result[x][y] = '*';
-			else {
+				return true;
+			} else {
 				int total = 0;
-				for (int i = Math.max(0, x); i < Math.min(x + 3, board.length); i++)
-					for (int j = Math.max(0, y); j < Math.min(y + 3, board.length); j++)
-						total++;
+				for (int i = Math.max(0, x - 1); i < Math.min(x + 2, board.length); i++)
+					for (int j = Math.max(0, y - 1); j < Math.min(y + 2, board.length); j++)
+						if (board[i][j] == '*')
+							total++;
 				result[x][y] = (total + "").charAt(0);
 				if (total == 0)
-					for (int i = Math.max(0, x); i < Math.min(x + 3, board.length); i++)
-						for (int j = Math.max(0, y); j < Math.min(y + 3, board.length); j++)
+					for (int i = Math.max(0, x - 1); i < Math.min(x + 2, board.length); i++)
+						for (int j = Math.max(0, y - 1); j < Math.min(y + 2, board.length); j++)
 							doMove(board, result, i, j);
 			}
+		return false;
 	}
 
+	/**
+	 * The play method
+	 * 
+	 * @param board
+	 *            - the board to play the game on
+	 * @param moves
+	 *            - the moves to make
+	 * @return the result of playing the passed moves
+	 */
 	public static char[][] playMoves(char[][] board, int[][] moves) {
 		char[][] result = new char[board.length][board[0].length];
-		for (char[] row : result)
-			for (char c : row)
-				row[c] = '_';
+		for (char[] column : result)
+			for (int i = 0; i < column.length; i++)
+				column[i] = '_';
 		for (int[] move : moves)
-			doMove(board, result, move[0], move[1]);
-		System.out.println(Arrays.deepToString(result));
+			if (doMove(board, result, move[0], move[1]))
+				break;
 		return result;
 	}
 }
--- Timing +Testing/src/RecursionMatricesTests.java
+++ Timing +Testing/src/RecursionMatricesTests.java
@@ -11,38 +11,57 @@ public class RecursionMatricesTests {
 				{ '*', ' ', '*', ' ', ' ', ' ' }, { '*', '*', '*', ' ', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ', ' ' },
 				{ ' ', '*', '*', '*', ' ', '*' } };
 		; // given example
-
 		int[][] moves11 = { { 0, 0 }, { 0, 2 } };
-		int[][] moves12 = {};
-		int[][] moves13 = { { 5, 1 }, { 0, 0 } };
-		int[][] moves14 = { { 3, 5 }, { 5, 0 } };
-
 		char[][] expected11 = { { '2', '_', '2', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '_', '_', '_' } };
+		char[][] res11 = Question6.playMoves(board1, moves11);
+		assertTrue(Arrays.deepEquals(expected11, res11));
+	}
 
+	@Test
+	public void testQuestion6_2() {
+		char[][] board1 = { 
+				{ ' ', '*', ' ', ' ', '*', '*' },
+				{ '*', ' ', ' ', '*', '*', ' ' },
+				{ '*', ' ', '*', ' ', ' ', ' ' }, 
+				{ '*', '*', '*', ' ', ' ', ' ' }, 
+				{ ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', '*', '*', '*', ' ', '*' } };
+		; // given example
+		int[][] moves12 = {};
 		char[][] expected12 = { { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '_', '_', '_' } };
+		char[][] res12 = Question6.playMoves(board1, moves12);
+		assertTrue(Arrays.deepEquals(expected12, res12));
+	}
 
+	@Test
+	public void testQuestion6_3() {
+		char[][] board1 = { { ' ', '*', ' ', ' ', '*', '*' }, { '*', ' ', ' ', '*', '*', ' ' },
+				{ '*', ' ', '*', ' ', ' ', ' ' }, { '*', '*', '*', ' ', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', '*', '*', '*', ' ', '*' } };
+		; // given example
+		int[][] moves13 = { { 5, 1 }, { 0, 0 } };
 		char[][] expected13 = { { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '*', '_', '_', '_', '_' } };
+		char[][] res13 = Question6.playMoves(board1, moves13);
+		assertTrue(Arrays.deepEquals(expected13, res13));
+	}
 
+	@Test
+	public void testQuestion6_4() {
+		char[][] board1 = { { ' ', '*', ' ', ' ', '*', '*' }, { '*', ' ', ' ', '*', '*', ' ' },
+				{ '*', ' ', '*', ' ', ' ', ' ' }, { '*', '*', '*', ' ', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', '*', '*', '*', ' ', '*' } };
+		; // given example
+		int[][] moves14 = { { 3, 5 }, { 5, 0 } };
 		char[][] expected14 = { { '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_' },
 				{ '_', '_', '_', '4', '2', '1' }, { '_', '_', '_', '2', '0', '0' }, { '_', '_', '_', '3', '2', '1' },
 				{ '1', '_', '_', '_', '_', '_' } };
-
-		char[][] res11 = Question6.playMoves(board1, moves11);
-		char[][] res12 = Question6.playMoves(board1, moves12);
-		char[][] res13 = Question6.playMoves(board1, moves13);
 		char[][] res14 = Question6.playMoves(board1, moves14);
-
-		assertTrue(Arrays.deepEquals(expected11, res11));
-		assertTrue(Arrays.deepEquals(expected12, res12));
-		assertTrue(Arrays.deepEquals(expected13, res13));
 		assertTrue(Arrays.deepEquals(expected14, res14));
-
 	}
-
 }
