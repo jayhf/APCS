92fc75abd46a54f147339df5d97b1741c6fd261f
Jay Fleischer <jayhf614@gmail.com>
2014-04-17 22:08:35 -0400

    Fixed the files to get rid of every \r (I hope)

--- Trees/src/DataCompression.java
+++ Trees/src/DataCompression.java
@@ -2,9 +2,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class DataCompression {
-	
 	public static void main(String[] args) throws IOException {
-		File f = new File("files/test.pre");
+		File f = new File("files/pi.pre");
 		System.out.println(FileCompressor.decompress(f));
 	}
 }
