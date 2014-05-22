69ce29c5c0d579951225784a85a287389770dd7e
Jay Fleischer <jayhf614@gmail.com>
2014-05-22 07:25:39 -0400

    Made progress on multiple path finding

--- RogueDist/MonsterRogueUtils.java
+++ RogueDist/MonsterRogueUtils.java
@@ -1,17 +1,20 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class MonsterRogueUtils {
-	public static Iterable<Site> adjacentSites(Site site) {
+	public static List<Site> adjacentSites(Site site) {
 		int c = site.col();
 		int r = site.row();
-		return Arrays.asList(new Site[] { new Site(r + 1, c + 1), new Site(r - 1, c + 1),
+		return Arrays.asList(new Site[] {new Site(r + 1, c + 1), new Site(r - 1, c + 1),
 				new Site(r - 1, c - 1), new Site(r + 1, c - 1), new Site(r + 1, c),
-				new Site(r, c + 1), new Site(r - 1, c), new Site(r, c - 1) });
+				new Site(r, c + 1), new Site(r - 1, c), new Site(r, c - 1)});
 	}
-	
+
 	public static LinkedGraph<Site> parseDungeon(Dungeon dungeon) {
 		LinkedGraph<Site> graph = new LinkedGraph<Site>();
 		for (int x = 0; x < dungeon.size(); x++)
@@ -26,12 +29,10 @@ public class MonsterRogueUtils {
 					graph.addDirectedEdge(site, adjacent);
 		return graph;
 	}
-	
+
 	public static LinkedList<LinkedList<Site>> shortestPaths(Site start, Site finish, LinkedGraph<Site> sites) {
-		LinkedList<LinkedList<Site>> pathList = new LinkedList<LinkedList<Site>>();
-		HashMap<Site, LinkedList<Site>> visited = new HashMap<Site, LinkedList<Site>>();
 		HashMap<Site, Integer> distancesAway = new HashMap<Site, Integer>();
-		visited.put(start, null);
+		distancesAway.put(start, 0);
 		Queue<Site> nearby = new LinkedList<Site>();
 		Queue<Site> nextToCheck = new LinkedList<Site>();
 		nearby.add(start);
@@ -44,28 +45,37 @@ public class MonsterRogueUtils {
 			while (!nearby.isEmpty()) {
 				Site site = nearby.poll();
 				if (site.equals(finish)) {
-					Site previous = site;
-					LinkedList<Site> path = new LinkedList<Site>();
-					path.add(finish);
-					while (!visited.get(previous).equals(start)) {
-						previous = visited.get(previous);
-						path.addFirst(previous);
-					}
-					path.addFirst(start);
-					pathList.add(path);
-					visited.remove(finish);
 					resultFound = true;
+					break;
 				}
-				if (!resultFound)
+				else
 					for (Site adjacent : sites.adjacentTo(site))
-						if (!visited.containsKey(adjacent) || distancesAway.get(site)) {
+						if (!distancesAway.containsKey(adjacent)) {
 							nextToCheck.add(adjacent);
-							
-							visited.put(adjacent, site);
+							distancesAway.put(adjacent, currentDepth);
 						}
 			}
 		}
-		System.out.println(pathList);
-		return pathList;
+		if (resultFound) {
+			LinkedList<LinkedList<Site>> pathList = new LinkedList<LinkedList<Site>>();
+			pathList.add(new LinkedList<Site>(Arrays.asList(finish)));
+			for(currentDepth--;currentDepth>0;currentDepth--) {
+				ListIterator<LinkedList<Site>> itr=pathList.listIterator();
+				while(itr.hasNext()) {
+					LinkedList<Site> path=itr.next();
+					List<Site> adjacent=adjacentSites(path.getFirst());
+					Iterator<Site> adjacentItr=adjacent.iterator();
+					while(adjacentItr.hasNext())
+						
+				}
+			}
+			
+			path.addFirst(start);
+			pathList.add(path);
+			visited.remove(finish);
+			System.out.println(pathList);
+			return pathList;
+		} else
+			return null;
 	}
 }
