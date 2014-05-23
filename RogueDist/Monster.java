7aab9ef38564554c1469343b1aad6bba0b927d77
Jay Fleischer <fleischerj@robotics-2.ntd.horacemann.org>
2014-05-23 11:52:32 -0400

    Monster somewhat works

--- RogueDist/Monster.java
+++ RogueDist/Monster.java
@@ -1,4 +1,5 @@
-import java.util.Set;
+import java.util.Collections;
+import java.util.Map;
 
 public class Monster implements MoveFinder {
 	private LinkedGraph<Site> graph;
@@ -14,14 +15,14 @@ public class Monster implements MoveFinder {
 
 	@Override
 	public Site move(Site monster, Site rogue) {
-		Set<Site> s = MonsterRogueUtils..reachableSites(monster, graph);
+		Map<Site,Integer> s = MonsterRogueUtils.createDistanceMap(monster, Collections.<Site>emptyList(), graph);
 		/*for (int r = 0; r < 10; r++) {
 			for (int c = 0; c < 10; c++)
 				System.out.print(s.contains(new Site(r, c)) ? 1 : " ");
 			System.out.println();
 		}
 		System.out.println(MonsterRogueUtils.distance(monster, rogue, graph));*/
-		if(!s.contains(rogue))
+		if(!s.containsKey(rogue))
 			return monster;
 		return MonsterRogueUtils.shortestPaths(monster, rogue, graph).get(0).get(1);
 	}
